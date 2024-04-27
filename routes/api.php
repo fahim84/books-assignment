9d29c9a19406a9540cb8e50016c139d4ccf47db1
Fahim Ahmed <fahim.ahmed@freeman.com>
2024-04-27 15:56:38 +0500

    Laravel Sanctum added to API routes

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,37 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('/books',BookController::class);
+Route::apiResource('/books',BookController::class)->middleware('auth:sanctum');
+
+Route::post('/sanctum/token', function (Request $request) {
+
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    return $user->createToken($request->device_name)->plainTextToken;
+});
+
+Route::post('/sanctum/test', function (Request $request) {
+
+    echo 'ttest';
+
+});
