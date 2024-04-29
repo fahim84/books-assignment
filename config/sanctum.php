2cbb7f24d87d146788a0b7ed54ae1dfd9f60167c
Fahim Ahmed <fahim.ahmed@freeman.com>
2024-04-29 13:52:51 +0500

    resolved cors error

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'http://books.local,localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        'http://books.local,http://localhost:5173,localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
         Sanctum::currentApplicationUrlWithPort()
     ))),
 
