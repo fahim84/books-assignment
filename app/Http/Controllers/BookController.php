48e352e82542270b93c6db2e5542b1fc41db5f05
Fahim Ahmed <fahim.ahmed@freeman.com>
2024-04-29 16:16:49 +0500

    resolved cors error

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -22,7 +22,7 @@ class BookController extends Controller
         header('Access-Control-Allow-Origin: *');
         header('Content-Type: application/json');
         header('Access-Control-Allow-Headers: Content-Type');
-        header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+        header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS');
         header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
     }
     /**
